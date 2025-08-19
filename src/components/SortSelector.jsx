6e0f56779f30cceba178ff7f025bf3512637bd9c
darkosel3 <darkosel7@gmail.com>
2025-08-19 21:05:30 +0200

    Small react project that uses Crypto Api to represent current values

---
+++ src/components/SortSelector.jsx
@@ -0,0 +1,21 @@
+const SortSelector = ({ sortBy, onSortChange }) => {
+  return (
+    <div className="controls">
+      <label htmlFor="sort">Sort By:</label>
+      <select
+        id="sort"
+        value={sortBy}
+        onChange={(e) => onSortChange(e.target.value)}
+      >
+        <option value="market_cap_desc">Market cap (High To Low)</option>
+        <option value="market_cap_asc">Market cap (Low To High)</option>
+        <option value="price_desc">Price (High To Low)</option>
+        <option value="price_asc">Price (Low To High)</option>
+        <option value="change_desc">24h Change (Low To High)</option>
+        <option value="change_asc">24 Change (High To Low)</option>
+      </select>
+    </div>
+  );
+};
+
+export default SortSelector;
